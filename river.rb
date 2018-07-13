66b058c9003def63b1d3616a2917e4fbea50f022
isabellademetz <isabella21136@gmail.com>
2018-07-13 16:36:36 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -1,3 +1,13 @@
 # river.rb
 class River
   def initialize(name, length, countries, discharge)
+    @name = name
+    @length = length
+    @countries = countries
+    @discharge = discharge
+    
+    def flood
+      discharge*.3 + discharge
+    end
+    def dry_up
+      dischage*
